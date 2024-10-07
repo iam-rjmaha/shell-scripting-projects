f41e0d4a4eb7bc70d175049c5b1584527fb85538
Mahalakshmi R <mahalakshmircse99@gmail.com>
2024-10-07 10:20:51 +0530

    Update aws_resources_usage_tracker.sh

--- aws_resources_usage_tracker.sh
+++ aws_resources_usage_tracker.sh
@@ -1,38 +1,37 @@
 #!/bin/bash
-#
-#
-####################3
+
+####################
 #Author:Mahalakshmi
 #Date:06/10/2024
-#
-#
-#
-#
 #Version:V1
-#
-#
+#####################
+
 #This script will report the usage of aws resorces
-#
-#
+#Resources to be track
 #AWS S3
 #AWS EC2
 #AWS lambda
 #AWS IAM users
-#
+
+#Define output file
+OUTPUT_FILE="aws_usage_report.txt"
 #
 #
 set -x
 
 echo "print s3 bucket list"
-aws s3 ls
+aws s3 ls > "$OUTPUT_FILE"
 
-aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId'
+aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId' > "$OUTPUT_FILE"
 
 
 
-aws lambda list-functions
+aws lambda list-functions > "$OUTPUT_FILE"
 
-aws iam list-users
+aws iam list-users > "$OUTPUT_FILE"
+
+# Add a timestamp
+echo "Report generated on: $(date)" >> "$OUTPUT_FILE"
 ~
 ~
 ~
